745b51fb6dd35de34daf17028eb2998b1eda622d
LJTDiSZ <jiang.github@163.com>
2015-07-13 14:15:10 +0800

    Add templates
    templates

--- article/models.py
+++ article/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
+from django.core.urlresolvers import reverse
 
 # Create your models here.
 class Article(models.Model):
@@ -9,6 +10,11 @@ class Article(models.Model):
 	date_time = models.DateTimeField(auto_now_add=True)
 	content = models.TextField(blank=True, null=True)
 
+	#获取URL并转换成url的表示格式
+	def get_absolute_url(self):
+		path = reverse('detail', kwargs={'id':self.id})
+		return "http://localhost:8000%s" % path
+	
 	#python2使用__unicode__, python3使用__str__
 	def __str__(self):
 		return self.title		
